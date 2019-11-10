494e1a5d4c198a25b42c1d817b360a5e5c302559
Chris Ferber <chris@ferbers.us>
2019-11-09 21:50:42 -0600

    finish movie route to store all data from movies table to redux store

--- server/routes/movie.router.js
+++ server/routes/movie.router.js
@@ -4,13 +4,13 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    // const queryText = 'SELECT * FROM plant';
-    // pool.query(queryText)
-    //   .then((result) => { res.send(result.rows); })
-    //   .catch((err) => {
-    //     console.log('Error completing SELECT plant query', err);
-    //     res.sendStatus(500);
-    //   });
+    const queryText = 'SELECT * FROM movies';
+    pool.query(queryText)
+      .then((result) => { res.send(result.rows); })
+      .catch((err) => {
+        console.log('Error completing SELECT plant query', err);
+        res.sendStatus(500);
+      });
     console.log('movie router was hit');
     
   });
